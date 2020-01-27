eb23309564ad877fc661c94a94be4bdc420c96a5
Den4eg <notparsed@gmail.com>
2020-01-27 11:54:57 +0300

    animate css add
    Signed-off-by: Den4eg <notparsed@gmail.com>

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,49 +1,52 @@
 <template>
-	<transition-group name="newT" tag="div">
-		<ticket
-			:ticketValues="ticket"
-			v-for="ticket in dbValues"
-			:key="ticket.ticketNumber"
-		></ticket>
-	</transition-group>
+    <transition-group
+        name="newT"
+        enter-active-class="animated bounceInLeft slow"
+        tag="div"
+    >
+        <ticket
+            :ticketValues="ticket"
+            v-for="ticket in dbValues"
+            :key="ticket.ticketNumber"
+        ></ticket>
+    </transition-group>
 </template>
 <script>
-import Ticket from "./contentModules/Ticket.vue";
+import Ticket from './contentModules/Ticket.vue';
 
 export default {
-	components: {
-		ticket: Ticket,
-	},
-	data: function() {
-		return {};
-	},
-	methods: {},
-	computed: {
-		dbValues() {
-			return this.$store.getters.transportOnTer;
-		},
-	},
+    components: {
+        ticket: Ticket
+    },
+    data: function() {
+        return {};
+    },
+    methods: {},
+    computed: {
+        dbValues() {
+            return this.$store.getters.transportOnTer;
+        }
+    }
 };
 </script>
 <style scoped>
 .newT-move {
-	transition: 0.8s all 0.4s;
-	position: relative;
+    transition: 0.8s all 0.4s;
 }
-.newT-enter-active {
-	transition: all 1s ease 0.8s;
+/* .newT-enter-active {
+    transition: all 1s ease 0.8s;
 }
 .newT-leave-active {
-	transition: all 1s;
-	position: absolute;
+    transition: all 1s;
+    position: absolute;
 }
 
 .newT-leave-to {
-	transform: translateX(-200px);
-	opacity: 0;
+    transform: translateX(-200px);
+    opacity: 0;
 }
 .newT-enter {
-	transform: translateX(-200px);
-	opacity: 0;
-}
+    transform: translateX(-200px);
+    opacity: 0;
+} */
 </style>
